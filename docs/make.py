60e7a6d12d0f1c51b89fea611ceda2bbf044a47d
Jonathan Marks <88326588+jon-marks@users.noreply.github.com>
2023-12-18 20:00:23 +0200

    Interim Checkin. * Cleanup - some documentation updates.

--- docs/make.py
+++ docs/make.py
@@ -2,11 +2,11 @@ import os
 from git import Repo
 from time import strftime, localtime
 
-os.system(".\\make.bat clean")
 oRepo = Repo("..\\")
 gComTag = oRepo.head.commit.hexsha[:7]
 gDirty = "*" if oRepo.is_dirty else ""
 gComTime = strftime("%a, %d %b %Y, %H:%M:%S", localtime(oRepo.head.commit.committed_date))
+os.system(".\\make.bat clean")
 f = open('build\\copyright', 'wt')
 f.write('2023, Jonathan Marks, Commit Tag: ' + gComTag + gDirty + ', ' + gComTime)
 f.close()
