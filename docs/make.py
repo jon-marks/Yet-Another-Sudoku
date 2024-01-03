bdb5d8e4264b1da3b531facd5270aa60890a8625
Jon Marks <jm@cmex.org>
2024-01-03 10:03:13 +0200

    Interim Checkin. Fixed copyright year and added images to the repository.

--- docs/make.py
+++ docs/make.py
@@ -1,6 +1,8 @@
 import subprocess
 from git import Repo
 from time import strftime, localtime
+import datetime
+from datetime import datetime
 
 oRepo = Repo("..\\")
 gComTag = oRepo.head.commit.hexsha[:7]
@@ -8,6 +10,6 @@ gDirty = "*" if oRepo.is_dirty() else ""
 gComTime = strftime("%a, %d %b %Y, %H:%M:%S", localtime(oRepo.head.commit.committed_date))
 subprocess.run(".\\make.bat clean")
 f = open('build\\copyright', 'wt')
-f.write('2023, Jonathan Marks, Commit Tag: ' + gComTag + gDirty + ', ' + gComTime)
+f.write(f'{datetime.now().year}, Jonathan Marks, Commit Tag: ' + gComTag + gDirty + ', ' + gComTime)
 f.close()
 subprocess.run(".\\make.bat html")
